75b0e1a985cd95dae5e5e773193ab57d908c702c
ujjwalmanandhar <ujjwal.manandhar@fusemachines.com>
2024-08-05 14:37:22 +0545

    add events,sendEvent, eventemitter to listen to change theme value

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 import * as ExpoSettings from "expo-settings";
+import { useEffect, useState } from "react";
+
+export type ThemeType = "light" | "dark" | "system";
 
 export default function App() {
+  const [theme, setTheme] = useState<ThemeType>(ExpoSettings.getTheme());
+
+  const nextTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const subscription = ExpoSettings.addThemeChangeListener(({ theme }) => {
+      setTheme(theme);
+    });
+    return () => {
+      subscription.remove();
+    };
+  });
   return (
     <View style={styles.container}>
-      <Text>{ExpoSettings.getTheme()}</Text>
+      <Text>Theme: {theme}</Text>
+      <Button
+        title={`Change Theme to ${nextTheme}`}
+        onPress={() => ExpoSettings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
