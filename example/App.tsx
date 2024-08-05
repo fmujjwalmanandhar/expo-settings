11e5865b220ce35215a4f798cadad3696111c4e3
ujjwalmanandhar <ujjwal.manandhar@fusemachines.com>
2024-08-05 12:01:30 +0545

    clean up default module and run

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-import * as ExpoSettings from 'expo-settings';
+import * as ExpoSettings from "expo-settings";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{ExpoSettings.hello()}</Text>
+      <Text>{ExpoSettings.getTheme()}</Text>
     </View>
   );
 }
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
